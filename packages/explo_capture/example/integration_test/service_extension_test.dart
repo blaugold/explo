34c626e9d73a8d10780ec509f022ad3e9ae4e07a
Gabriel Terwesten <gabriel@terwesten.net>
2021-12-12 15:03:50 +0100

    test: add tests for `explo_capture` (#6)

---
+++ packages/explo_capture/example/integration_test/service_extension_test.dart
@@ -0,0 +1,103 @@
+import 'dart:developer';
+import 'dart:isolate';
+
+import 'package:explo_capture/explo_capture.dart';
+import 'package:explo_capture/internal.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:vm_service/vm_service.dart' as vm;
+import 'package:vm_service/vm_service_io.dart';
+
+late final String isolateId;
+late final vm.VmService vmService;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    isolateId = Service.getIsolateID(Isolate.current)!;
+    final serviceInfo = await Service.getInfo();
+    vmService =
+        await vmServiceConnectUri(serviceInfo.serverWebSocketUri.toString());
+    // Listen to extension events, so we receive render tree change events.
+    await vmService.streamListen(vm.EventStreams.kExtension);
+  });
+
+  tearDownAll(() async {
+    await vmService.dispose();
+  });
+
+  testWidgets(
+    'ext.explo.getRenderTree returns currently captured render tree',
+    (tester) async {
+      // Build the widget tree, with CaptureRenderTree to register the capture
+      // service extensions.
+      await tester.pumpWidget(CaptureRenderTree(child: Container()));
+
+      var renderTreeData = await getRenderTree();
+      expect(renderTreeData, isNotNull);
+      expect(renderTreeData!.type, 'RenderLimitedBox');
+
+      // Rebuild without CaptureRenderTree.
+      await tester.pumpWidget(Container());
+
+      // Without a CaptureRenderTree, no render tree should be captured.
+      renderTreeData = await getRenderTree();
+      expect(renderTreeData, isNull);
+    },
+  );
+
+  testWidgets(
+    'posts ext.explo.renderTreeChanged event after each frame',
+    (tester) async {
+      // Build the widget tree, with CaptureRenderTree to register the capture
+      // service extensions.
+      await tester.pumpWidget(CaptureRenderTree(child: Container()));
+
+      expect(
+        renderTreeChanges().map((event) => event?.type),
+        emitsInOrder(<Object>['RenderLimitedBox']),
+      );
+
+      // This frame wont trigger an event, because no listener is registered.
+      await tester.pumpWidget(CaptureRenderTree(child: Container()));
+
+      await updateRenderTreeChangeListeners(1);
+
+      // This frame will trigger an event, because a listener registered.
+      await tester.pumpWidget(CaptureRenderTree(child: Container()));
+
+      await updateRenderTreeChangeListeners(-1);
+
+      // This frame wont trigger an event, because no listener is registered.
+      await tester.pumpWidget(CaptureRenderTree(child: Container()));
+    },
+  );
+}
+
+Future<RenderObjectData?> getRenderTree() async {
+  final response = await vmService.callServiceExtension(
+    getRenderTreeMethod,
+    isolateId: isolateId,
+  );
+  return renderTreeFromJson(response.json);
+}
+
+Future<void> updateRenderTreeChangeListeners(int delta) {
+  return vmService.callServiceExtension(
+    updateRenderTreeChangeListenersMethod,
+    isolateId: isolateId,
+    args: <String, dynamic>{
+      'delta': delta,
+    },
+  );
+}
+
+Stream<RenderObjectData?> renderTreeChanges() {
+  return vmService.onExtensionEvent
+      .where((event) =>
+          event.isolate?.id == isolateId &&
+          event.extensionKind == renderTreeChangedEvent)
+      .map((event) => renderTreeFromJson(event.extensionData!.data));
+}
---
+++ packages/explo_capture/test/render_object_data_test.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:explo_capture/src/render_object_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+    'capture render tree data',
+    (tester) async {
+      await tester.binding.setSurfaceSize(const Size.square(200));
+
+      await tester.pumpWidget(
+        Center(
+          child: SizedBox.fromSize(
+            size: const Size.square(100),
+            child: const SizedBox(),
+          ),
+        ),
+      );
+
+      final root = tester.firstRenderObject(find.byType(Center));
+      final rootData = RenderObjectData.capture(root);
+      expect(rootData.parent, isNull);
+      expect(rootData.type, 'RenderPositionedBox');
+      expect(rootData.level, 0);
+      expect(rootData.paintBounds, const Rect.fromLTWH(0, 0, 200, 200));
+
+      final childData = rootData.children.first;
+      expect(childData.parent, rootData);
+      expect(childData.type, 'RenderConstrainedBox');
+      expect(childData.level, 1);
+      expect(childData.paintBounds, const Rect.fromLTWH(50, 50, 100, 100));
+
+      final grandChildData = childData.children.first;
+      expect(rootData.children, [childData]);
+      expect(childData.children, [grandChildData]);
+      expect(grandChildData.parent, childData);
+      expect(
+        rootData.descendants,
+        containsAll(<Object>[childData, grandChildData]),
+      );
+    },
+  );
+
+  test('serialize to and from JSON', () {
+    final a = RenderObjectData(
+      type: 'a',
+      paintBounds: const Rect.fromLTWH(1, 2, 3, 4),
+    );
+    final b = RenderObjectData(
+      type: 'b',
+      paintBounds: const Rect.fromLTWH(5, 6, 7, 8),
+    );
+    a.addChild(b);
+
+    final json = jsonDecode(jsonEncode(a.toJson())) as Map<String, Object?>;
+
+    final a_ = RenderObjectData.fromJson(json);
+    expect(a_.type, a.type);
+    expect(a_.level, a.level);
+    expect(a_.paintBounds, a.paintBounds);
+
+    final b_ = a_.children.first;
+    expect(b_.type, b.type);
+    expect(b_.level, b.level);
+    expect(b_.paintBounds, b.paintBounds);
+  });
+}
